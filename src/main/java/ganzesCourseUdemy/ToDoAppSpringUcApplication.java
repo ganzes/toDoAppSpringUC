ff04cd03b48ae26178a768d2b7bd732d9d3c6753
ganzes <charczuk.arkadiusz@gmail.com>
2020-09-02 19:24:27 +0200

    toDoAppSpring Udemy Course initial up

---
+++ src/main/java/ganzesCourseUdemy/ToDoAppSpringUcApplication.java
@@ -0,0 +1,13 @@
+package ganzesCourseUdemy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToDoAppSpringUcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToDoAppSpringUcApplication.class, args);
+	}
+
+}
