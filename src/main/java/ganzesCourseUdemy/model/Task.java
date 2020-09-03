0af7a7466f107d6d0a9c6635dff4ee30cd5f8aac
ganzes <charczuk.arkadiusz@gmail.com>
2020-09-03 23:06:54 +0200

    added strategy auto, and rid of warn open in view false

--- src/main/java/ganzesCourseUdemy/model/Task.java
+++ src/main/java/ganzesCourseUdemy/model/Task.java
@@ -1,13 +1,12 @@
 package ganzesCourseUdemy.model;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Entity
 @Table(name = "tasks")
 public class Task {
     @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private int id;
     //@Column(name = "description")
     private String description;
