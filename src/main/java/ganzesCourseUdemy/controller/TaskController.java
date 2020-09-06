ff25bf50d8701ddec8d2a32c2db2c0b3a292db2a
ganzes <charczuk.arkadiusz@gmail.com>
2020-09-06 23:36:58 +0200

    TaskController  upgrade

--- src/main/java/ganzesCourseUdemy/controller/TaskController.java
+++ src/main/java/ganzesCourseUdemy/controller/TaskController.java
@@ -7,8 +7,11 @@ import org.slf4j.LoggerFactory;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @RestController
@@ -32,5 +35,11 @@ public class TaskController {
     ResponseEntity<List<Task>> readAllTasks(Pageable page){
         logger.info("Custom pageable!");
         return ResponseEntity.ok(taskRepository.findAll(page).getContent());
+    }
+
+    @PutMapping("/tasks/{id}")
+    ResponseEntity<?> updateTask(@RequestBody @Valid Task toUpdate){
+        taskRepository.save(toUpdate);
+        return ResponseEntity.noContent().build();
     }
 }
