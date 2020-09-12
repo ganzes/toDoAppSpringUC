936628c002ab95e5ff9dd2562202b850cdd4603b
ganzes <charczuk.arkadiusz@gmail.com>
2020-09-12 19:21:01 +0200

    attempt to hierarchy

--- src/main/java/ganzesCourseUdemy/TaskConfigurationProperties.java
+++ src/main/java/ganzesCourseUdemy/TaskConfigurationProperties.java
@@ -1,18 +1,36 @@
 package ganzesCourseUdemy;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.NestedConfigurationProperty;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @ConfigurationProperties("task")
 public class TaskConfigurationProperties {
-    private boolean allowMultipleTaskFromTemplate;
+
+    @NestedConfigurationProperty
+    Template template = new Template();
+
+    //private boolean allowMultipleTaskFromTemplate;
 
     public boolean isAllowMultipleTaskFromTemplate() {
-        return allowMultipleTaskFromTemplate;
+        return template.allowMultipleTasks;
     }
 
     public void setAllowMultipleTaskFromTemplate(final boolean allowMultipleTaskFromTemplate) {
-        this.allowMultipleTaskFromTemplate = allowMultipleTaskFromTemplate;
+        this.template.allowMultipleTasks = allowMultipleTaskFromTemplate;
+    }
+
+    @ConfigurationProperties("template")
+    private class Template{
+        boolean allowMultipleTasks;
+
+        public boolean isAllowMultipleTasks() {
+            return allowMultipleTasks=true;
+        }
+
+        public void setAllowMultipleTasks(boolean allowMultipleTasks) {
+            this.allowMultipleTasks = allowMultipleTasks;
+        }
     }
 }
