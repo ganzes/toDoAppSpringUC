f73bce1d1e75b8369487290e2aa4d5baf19a8481
ganzes <charczuk.arkadiusz@gmail.com>
2020-09-22 16:50:40 +0200

    update Task and TaskGroup, JoinColumn, OneToMany, ManyToOne

---
+++ src/main/java/ganzesCourseUdemy/model/TaskGroup.java
@@ -0,0 +1,53 @@
+package ganzesCourseUdemy.model;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import java.util.Set;
+
+@Entity
+@Table(name = "task_groups")
+public class TaskGroup {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    @NotBlank(message = "Tasks description must not be null and empty")
+    private String description;
+    private boolean done;
+    @OneToMany(fetch = FetchType.LAZY, cascade = CascadeType.ALL, mappedBy = "group")
+    private Set<Task> tasks;
+
+    public TaskGroup() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public boolean isDone() {
+        return done;
+    }
+
+    public void setDone(boolean done) {
+        this.done = done;
+    }
+
+    public Set<Task> getTasks() {
+        return tasks;
+    }
+
+    public void setTasks(final Set<Task> tasks) {
+        this.tasks = tasks;
+    }
+}
