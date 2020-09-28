199059e8972d07d1f5a8a737695c6a09f72298c5
ganzes <charczuk.arkadiusz@gmail.com>
2020-09-28 03:45:30 +0200

    added ProjectService and 2 methods

---
+++ src/main/java/ganzesCourseUdemy/logic/ProjectService.java
@@ -0,0 +1,22 @@
+package ganzesCourseUdemy.logic;
+
+import ganzesCourseUdemy.model.Project;
+import ganzesCourseUdemy.model.ProjectRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ProjectService {
+
+    private ProjectRepository projectRepository;
+
+
+    public List<Project> findAll(){
+        return projectRepository.findAll();
+    };
+
+    public Project saveProject(Project project){
+        return projectRepository.save(project);
+    }
+}
